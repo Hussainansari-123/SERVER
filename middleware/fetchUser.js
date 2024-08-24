f405edcfd41834b20dcbb84bd7fa6151aba21e84
Hussain <sarifansari660@gmail.com>
2024-08-24 12:56:06 +0300

    Backendcommit

---
+++ middleware/fetchUser.js
@@ -0,0 +1,21 @@
+var jwt = require("jsonwebtoken");
+const JWT_SECRET = "Sarifisagoodb$oy";
+const fetchUser = (req, res, next) => {
+  //get the user from the jwt token and add id to the req object
+  const token = req.header("auth-token");
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
+  }
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
+  }
+};
+module.exports = fetchUser;
